74f51f4206cd2806dbb962c4cab310d97f93fb62
SohnEugene <102849934+SohnEugene@users.noreply.github.com>
2023-04-04 11:01:39 +0900

    5주차 추가

---
+++ 5주차/class/basic.js
@@ -0,0 +1,27 @@
+// 클래스 정의하기
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    console.log(`안녕하세요, ${this.name}입니다.`);
+  }
+
+  sayAge() {
+    console.log(`제 나이는 ${this.age}살 입니다.`)
+  }
+}
+
+// 객체 생성하기
+const person1 = new Person("Kim", 24);
+console.log(person1.name); // Kim
+person1.sayHello(); // 안녕하세요, Kim입니다.
+person1.sayAge();
+
+// 객체 생성하기
+const person2 = new Person("Jim", 24);
+console.log(person2.name); // Kim
+person2.sayHello(); // 안녕하세요, Kim입니다.
+person2.sayAge();
---
+++ 5주차/class/inheritance.js
@@ -0,0 +1,54 @@
+// 부모 클래스 정의하기
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name}이(가) 소리를 냅니다.`);
+  }
+}
+
+// 자식 클래스 정의하기
+class Dog extends Animal {
+  constructor(name) {
+    super(name);
+  }
+
+  speak() {
+    console.log(`${this.name}이(가) 멍멍 짖습니다.`);
+  }
+
+  fetch() {
+    console.log(`${this.name}이(가) 공을 가져옵니다.`);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+  }
+
+  speak() {
+    console.log(`${this.name}이(가) 야옹 웁니다.`);
+  }
+
+  sleep() {
+    console.log(`${this.name}이(가) 쿨쿨 잡니다.`);
+  }
+
+  fight(Dog) {
+    console.log(`${this.name}와 ${Dog.name}가 싸웁니다.`);
+  }
+}
+
+// 객체 생성하기
+const dog = new Dog("뽀삐");
+dog.speak(); // 뽀삐이(가) 멍멍..
+dog.fetch(); // 뽀삐이(가) 공을..
+
+const cat = new Cat("네로");
+cat.speak();
+cat.sleep();
+
+cat.fight(dog);
